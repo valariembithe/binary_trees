26c4cf1f7fcb7b6b8550b2e7ddaa84a8edcd9524
valariembithe <valmbithe691@gmail.com>
2023-08-29 05:04:21 -0700

    files

--- binary_trees.h
+++ binary_trees.h
@@ -31,5 +31,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node); /* Task 5*/
+
+size_t binary_tree_size(const binary_tree_t *tree); /* 11 */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 #endif
