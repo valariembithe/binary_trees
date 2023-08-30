1e4e5ddd07ac5cfb3b25f59c273c11e964303954
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-31 01:01:45 +0300

    ✨✨✨ binary trees height

--- binary_trees.h
+++ binary_trees.h
@@ -35,7 +35,7 @@ int binary_tree_is_root(const binary_tree_t *node); /* Task 5*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-
+size_t binary_tree_height(const binary_tree_t *tree);
 
 
 size_t binary_tree_size(const binary_tree_t *tree); /* 11 */
