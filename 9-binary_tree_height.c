f7b361186fc8ac2b1a3298aa1101a948ac647b45
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-31 02:02:46 +0300

    🚑️🚑️🚑️ critical hotfix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,5 +13,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height) ? left_height : right_height);
+	return (1 + ((left_height > right_height) ?
+		     left_height : right_height));
 }
