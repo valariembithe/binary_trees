e4fcd3ae7776a13898653b1deadc96da394fec34
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-31 00:09:06 +0300

    ✨✨✨ implemented task 6

--- binary_trees.h
+++ binary_trees.h
@@ -32,6 +32,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node); /* Task 5*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+
+
 
 size_t binary_tree_size(const binary_tree_t *tree); /* 11 */
 size_t binary_tree_leaves(const binary_tree_t *tree);
