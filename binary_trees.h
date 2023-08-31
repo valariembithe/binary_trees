f20c969f9adb01fea84292ea8041651c17cfcc45
valariembithe <valmbithe691@gmail.com>
2023-08-31 03:22:39 -0700

    Tasks

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 bst_t *bst_insert(bst_t **tree, int value);
 int binary_tree_is_bst(const binary_tree_t *tree);
+int less_than(const binary_tree_t *tree, int val);
+int greater_than(const binary_tree_t *tree, int val);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
