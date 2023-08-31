b00ea621c60e12729d732ec97a75eb172c4c9de2
valariembithe <valmbithe691@gmail.com>
2023-08-31 02:40:34 -0700

    Tasks

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -13,6 +13,13 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 
 	if (tree->right)
 	{
-	}
+		tree->right = tree->right->left;
+		tree->parent->left = tree;
+		tree->parent->parent = NULL;
 
+		if (tree->right)
+			tree->right->parent = tree;
+		return (tree->parent);
+	}
+	return (tree);
 }
