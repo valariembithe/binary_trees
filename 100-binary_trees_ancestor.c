f61853c4227ec2d0f61248befccd564ac6f9d0de
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-31 04:00:56 +0300

    ⚡️⚡️ make betty happy

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -12,19 +12,19 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	binary_tree_t *ancestor_first, *ancestor_second;
 
 	if (!first || !second)
-		return NULL;
+		return (NULL);
 	if (first == second)
-		return (binary_tree_t *)first;
+		return ((binary_tree_t *)first);
 
 	ancestor_first = first->parent;
 	ancestor_second = second->parent;
 
 	if (first == ancestor_second || !ancestor_first ||
 	    (!ancestor_first->parent && ancestor_second))
-		return binary_trees_ancestor(first, ancestor_second);
+		return (binary_trees_ancestor(first, ancestor_second));
 	else if (ancestor_first == second || !ancestor_second ||
 		 (!ancestor_second->parent && ancestor_first))
-		return binary_trees_ancestor(ancestor_first, second);
+		return (binary_trees_ancestor(ancestor_first, second));
 
-	return binary_trees_ancestor(ancestor_first, ancestor_second);
+	return (binary_trees_ancestor(ancestor_first, ancestor_second));
 }
